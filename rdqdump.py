152adfeb327a4538ec16b81f5a19bc1a50006f00
r2ro <arturo@nohasmith.com>
2017-11-16 13:54:09 +0000

    Update rdqdump.py
    Add output file option

--- rdqdump.py
+++ rdqdump.py
@@ -34,7 +34,7 @@ def readChunk(data,start,end):
 def hexbytes(xs, group_size=1, byte_separator=' ', group_separator=' '):
     def ordc(c):
         return ord(c) if isinstance(c,str) else c
-    
+
     if len(xs) <= group_size:
         s = byte_separator.join('%02X' % (ordc(x)) for x in xs)
     else:
@@ -51,13 +51,13 @@ def hexbytes(xs, group_size=1, byte_separator=' ', group_separator=' '):
 def hexprint(xs):
     def chrc(c):
         return c if isinstance(c,str) else chr(c)
-    
+
     def ordc(c):
         return ord(c) if isinstance(c,str) else c
-    
+
     def isprint(c):
         return ordc(c) in range(32,127) if isinstance(c,str) else c > 31
-    
+
     return ''.join([chrc(x) if isprint(x) else '.' for x in xs])
 
 
@@ -76,14 +76,14 @@ def hexdump(xs, group_size=4, byte_separator=' ', group_separator='-', printable
             bytes = hexbytes(chunk, group_size, byte_separator, group_separator)
             s += (address_format + ': %s%s\n') % (address + offset, bytes, printable_separator + hexprint(chunk) if printable_separator else '')
             bytes_len = len(bytes)
-        
+
         if r > 0:
             offset = len(xs)-r
             chunk = xs[offset:offset+r]
             bytes = hexbytes(chunk, group_size, byte_separator, group_separator)
             bytes = bytes + ' '*(bytes_len - len(bytes))
             s += (address_format + ': %s%s\n') % (address + offset, bytes, printable_separator + hexprint(chunk) if printable_separator else '')
-    
+
     return s
 
 def convert_hex(string):
@@ -99,20 +99,21 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-b", dest='bytes'  , default=16, type="int", help="number of bytes to show per line")
     parser.add_option("-s", dest='start' , default=0, type="int", help="starting byte")
-    parser.add_option("-l", dest='length' , default=16, type="int", help="length in bytes to dump")     
+    parser.add_option("-l", dest='length' , default=16, type="int", help="length in bytes to dump")
     parser.add_option("-r", dest='chunk' , default=1024, type="int", help="length in bytes to read at a time")
     parser.add_option("-f", dest='input', default="",help="input: filename")
     parser.add_option("-t", dest='text', default="",help="text string to search for")
-    
+    parser.add_option("-o", dest='output', help="output: filename")
+
     # this hex value worked for me, might work for you
     # to delimit the entries in a rabbitmq .rdq file
-    parser.add_option("-x", dest='hex', default="395f316c000000016d0000",help="hex string to search for")    
+    parser.add_option("-x", dest='hex', default="395f316c000000016d0000",help="hex string to search for")
     parser.add_option("-c", dest='count', default=1 ,type="int",help="count of hits to find before stopping (0 for don't stop)")
-    parser.add_option("-d", "--debug",action="store_true", dest="debug", default=False, help="turn on debugging output")    
-    parser.add_option("-z", "--zero",action="store_true", dest="zero", default=False,help="when printing output, count from zero rather than position hit was found")    
+    parser.add_option("-d", "--debug",action="store_true", dest="debug", default=False, help="turn on debugging output")
+    parser.add_option("-z", "--zero",action="store_true", dest="zero", default=False,help="when printing output, count from zero rather than position hit was found")
 
     (options,args) = parser.parse_args()
-    
+
 
     if os.path.exists(options.input):
         src=file(options.input,'rb')
@@ -123,12 +124,18 @@ if __name__ == '__main__':
         sys.stderr.write("No input file specified\n")
         sys.exit()
 
+    if options.output is not None:
+        output_file = options.output
+        output = True
+    else:
+        output = False
+
     searchSize=max(len(options.text),len(options.hex)/2)
     data=readChunk(src,options.start,options.chunk)
     if options.debug:
-        print("[*] position: %d"%(src.tell())) 
+        print("[*] position: %d"%(src.tell()))
     count=0
-    while data: 
+    while data:
 
         if len(options.hex)>0 and options.hex.upper() in convert_hex(data):
             #where is the string in this chunk of data
@@ -141,9 +148,9 @@ if __name__ == '__main__':
             if options.zero:
                 #used to carve out a portion of a stream and save it via xxd -r
                 printAddress=0
-                
+
             #set the length
-            recordSize = readChunk(src, dataAddress + len(options.hex)/2,  2)           
+            recordSize = readChunk(src, dataAddress + len(options.hex)/2,  2)
             rdqEntryLength = int(convert_hex(recordSize), 16)
             # print('rdqEntryLength:', rdqEntryLength)
             options.length = rdqEntryLength
@@ -153,10 +160,18 @@ if __name__ == '__main__':
             data=readChunk(src,dataAddress + len(options.hex)/2 + 2,options.length)
             unescapedData = data.decode('ascii', 'ignore')
             try:
-                sys.stdout.write(json.loads(unescapedData)+'\n')
+                if output:
+                    with open(output_file, "a") as out_file:
+                        out_file.write(json.loads(unescapedData)+'\n')
+                else:
+                    sys.stdout.write(json.loads(unescapedData)+'\n')
 
             except Exception as e:
-                sys.stderr.write(unescapedData + '\n')
+                if output:
+                    with open(output_file, "a") as out_file:
+                        out_file.write(unescapedData + '\n')
+                else:
+                    sys.stderr.write(unescapedData + '\n')
                 pass
             if options.debug:
                 print(hexdump(data, byte_separator='', group_size=2, group_separator=' ', printable_separator='  ', address=printAddress, line_size=16,address_format='%07X'))
@@ -169,5 +184,3 @@ if __name__ == '__main__':
             data=readChunk(src,src.tell(),options.chunk)
             if options.debug:
                 print("[*] position: %d"%(src.tell()))
-
-        
